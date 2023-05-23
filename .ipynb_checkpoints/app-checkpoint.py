f2e46f9c44c0594715bb868e1cdf10e35ec1675e
Balder Bryn Morsund <Balder.Bryn.Morsund@norconsult.com>
2023-05-23 14:30:17 +0200

    netlify compat

--- .ipynb_checkpoints/app-checkpoint.py
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import math
+import os
 
 app = Flask(__name__)
 
@@ -37,9 +38,9 @@ def index():
             distance = calculate_distance(my_lat, my_lon, lat, lon)
             distances.append(((lat, lon), distance))
 
-        return render_template('index.html', distances=distances)
+        return render_template('result.html', distances=distances)
 
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
